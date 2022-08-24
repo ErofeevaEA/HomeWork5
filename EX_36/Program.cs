97aeee1e08fb83497d59aa1939da4f98952cc441
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-24 21:33:54 +0300

    Exercise 36

---
+++ EX_36/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.Write("Введите размерность массива: ");
+        int size = int.Parse(Console.ReadLine());
+        int[] arr = new int[size];
+        Random rnd = new Random();
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.Next(0, 100);
+            Console.Write(arr[i] + " ");
+
+        }
+        int sum = 0;
+       for (int i = 1; i < arr.Length; i+=2)
+
+            {
+
+                  sum += arr[i];
+                  
+            }
+            Console.WriteLine("\nСумма:" + sum);
+    }
+}
