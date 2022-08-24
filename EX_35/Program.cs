91afdb48932a5ba5237835dd124ec224741f7251
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-24 21:39:34 +0300

    Exercise 35

---
+++ EX_35/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        int[] arr = new int[123];
+        int i = 0;
+        Random rand = new Random();
+
+        for (i = 0; i < 123; i++)
+        {
+            arr[i] = new Random().Next(0, 123);
+
+            Console.Write(arr[i] + "  ");
+        }
+        int count = 0;
+
+        for (i = 0; i < 123; i++)
+        {
+            if ((arr[i] >= 10) && (arr[i] <= 99)) count++;
+        }
+
+        Console.WriteLine("\nКоличество элементов в диапазоне: " + count);
+        Console.Write(" ");
+    }
+}
